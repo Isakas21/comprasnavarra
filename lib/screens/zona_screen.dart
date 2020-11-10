8142d7a2fa88c4473646429a614c96f2ec67cee0
Isakas21 <inakibengo14@hotmail.com>
2020-11-10 13:19:49 +0100

    zona get Storage

--- lib/screens/zona_screen.dart
+++ lib/screens/zona_screen.dart
@@ -1,15 +1,29 @@
+import 'package:comprasnavarra/screens/localidad_screen.dart';
+import 'package:comprasnavarra/screens/tipo_screen.dart';
 import 'package:flutter/material.dart';
-import '../providers/data_provider.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:get/get.dart';
 import '../providers/data_provider.dart';
 
 class ZonaScreen extends StatelessWidget {
+  Map<String, Object> args = new Map<String, Object>();
+  final box = GetStorage();
   @override
   Widget build(BuildContext context) {
+    args = Get.arguments ?? new Map<String, Object>();
+  
     return Scaffold(
       appBar: AppBar(
         title: Text("Zonas de compra de navarra"),
       ),
       body: _listZonas(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Navigator.pop(context);
+          Get.offAll(ListaTiposScreen(), arguments: args);
+        },
+        child: Icon(Icons.arrow_back),
+      ),
     );
   }
 
@@ -35,6 +49,10 @@ class ZonaScreen extends StatelessWidget {
       final w = ListTile(
         title: Text(element),
         trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+          args['localidad'] = element;
+          Get.offAll(ListaLocalidadScreen(), arguments: args);
+        },
       );
       lst.add(w);
       lst.add(Divider());
